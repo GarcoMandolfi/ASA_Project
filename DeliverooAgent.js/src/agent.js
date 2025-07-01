ce5ae5e0739d58905833c1c0ae2c7edeafba9e4e
Marco Gandolfi <marcogandulf@gmail.com>
2025-07-01 15:11:12 +0200

    Removed API key

--- DeliverooAgent.js/src/agent.js
+++ DeliverooAgent.js/src/agent.js
@@ -2,7 +2,7 @@ import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
 
 const client = new DeliverooApi(
     'http://localhost:8080',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNiZGJmMSIsIm5hbWUiOiJUd29CYW5hbmFzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NTEzNjA2NDF9.J5uTBh3yTrUviXsl0o8djdHoMQ03tS0CE0lnJUDdKCE'
+    'GET YOUR OWN API KEY'
 )
 
 var me;
