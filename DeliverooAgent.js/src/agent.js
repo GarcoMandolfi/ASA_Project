11bc3cabfca35bd1b291635e80172a3e328e985c
Marco Gandolfi <marcogandulf@gmail.com>
2025-07-01 15:09:27 +0200

    First commit

---
+++ DeliverooAgent.js/src/agent.js
@@ -0,0 +1,16 @@
+import { DeliverooApi } from "@unitn-asa/deliveroo-js-client";
+
+const client = new DeliverooApi(
+    'http://localhost:8080',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjNiZGJmMSIsIm5hbWUiOiJUd29CYW5hbmFzIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NTEzNjA2NDF9.J5uTBh3yTrUviXsl0o8djdHoMQ03tS0CE0lnJUDdKCE'
+)
+
+var me;
+client.onYou( m => {
+    me = m;
+});
+
+client.onMap( (width, height, tiles) => {
+    console.log('Map of width ' + width + ' and height ' + height);
+})
+
